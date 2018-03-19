bbbfedb01bba26770272e6c1805bc72a924b33a3
Ando Munenori <ando@and-works.co.jp>
2018-03-19 15:53:22 +0900

    noteモデルを作成する。

---
+++ tests/Fixture/NotesFixture.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * NotesFixture
+ *
+ */
+class NotesFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'users_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'my_categories_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        '_indexes' => [
+            'my_categories_id' => ['type' => 'index', 'columns' => ['my_categories_id'], 'length' => []],
+            'users_id' => ['type' => 'index', 'columns' => ['users_id'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+            'notes_ibfk_1' => ['type' => 'foreign', 'columns' => ['my_categories_id'], 'references' => ['my_categories', 'id'], 'update' => 'noAction', 'delete' => 'noAction', 'length' => []],
+            'notes_ibfk_2' => ['type' => 'foreign', 'columns' => ['users_id'], 'references' => ['users', 'id'], 'update' => 'noAction', 'delete' => 'noAction', 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_unicode_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'users_id' => 1,
+            'my_categories_id' => 1,
+            'created' => '2018-03-19 15:52:35',
+            'modified' => '2018-03-19 15:52:35'
+        ],
+    ];
+}
